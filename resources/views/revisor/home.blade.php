40ba774a13fd5f5b018eaa4b7a28367384aa337b
BeaSerrano <beaserranolucio@gmail.com>
2021-04-06 18:26:58 +0200

    vista revisor home imagenes

--- resources/views/revisor/home.blade.php
+++ resources/views/revisor/home.blade.php
@@ -43,10 +43,23 @@
                         <div class="col-12 col-md-3 d-flex align-items-center">
                             <h3>{{__("ui.images")}}</h3>
                         </div>
-                        <div class="col-12 col-md-9">
+                        <div class="col-12">
                             @foreach ($announcement->images as $image)
-                            <div>
-                                <img class="border rounded-3 my-2 img-fluid" src="{{$image->getUrl(300,380)}}" alt="...">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <img class="border rounded-3 my-2 img-fluid" src="{{$image->getUrl(250,250)}}" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <p>Adult: {{$image->adult}}</p>
+                                    <p>Spoof: {{$image->spoof}}</p>
+                                    <p>Medical: {{$image->medical}}</p>
+                                    <p>Violence: {{$image->violence}}</p>
+                                    <p>Racy: {{$image->racy}}</p>
+
+                                    <p>Id: {{$image->id}}</p>
+                                    <p>Archivo: {{$image->file}}</p>
+                                    <p>URL: {{Storage::url($image->file)}}</p>
+                                </div>
                             </div>
                             @endforeach
                         </div>
